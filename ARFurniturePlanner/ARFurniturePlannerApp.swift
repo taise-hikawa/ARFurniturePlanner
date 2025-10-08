a66e2b714b367c7ab13dd3ffad4410018271bf30
taise-hikawa <cmmkarubi@gmail.com>
2025-10-09 00:19:45 +0900

    create project

---
+++ ARFurniturePlanner/ARFurniturePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFurniturePlannerApp.swift
+//  ARFurniturePlanner
+//
+//  Created by 樋川大聖 on 2025/10/09.
+//
+
+import SwiftUI
+
+@main
+struct ARFurniturePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
